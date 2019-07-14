a1699845a9196d1d5de601cbcd3c6835b572b0d0
iamgrawal <gaurav.rawal66@gmail.com>
2019-07-14 23:34:09 +0530

    input Added

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Layout } from 'antd';
+import UrlInput from '../../components/UrlInput';
 
 const { Header, Footer, Content } = Layout;
 
 function Dashboard(props) {
-  return (
-    <div>
-    <Layout>
-      <Header>Header</Header>
-      <Content>Content</Content>
-      <Footer>Footer</Footer>
-    </Layout>
-    </div>
-  )
+	return (
+		<div>
+			<Layout>
+				<Header>Header</Header>
+				<Content className="content">
+					<UrlInput />
+				</Content>
+				<Footer>Footer</Footer>
+			</Layout>
+		</div>
+	);
 }
 
-Dashboard.propTypes = {
-
-};
+Dashboard.propTypes = {};
 
 export default Dashboard;
-
