a6c4bd592e07ef9c4867dd524988ab6937ef5999
iamgrawal <gaurav.rawal66@gmail.com>
2019-07-14 19:15:50 +0530

    Add: React Router, Jest

---
+++ src/containers/Redirect/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Redirect(props) {
+  return (
+    <div>
+      Redirect
+    </div>
+  )
+}
+
+Redirect.propTypes = {
+
+};
+
+export default Redirect;
+
