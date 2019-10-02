f7a991f6ab0d402546fc860a5dd605704522e6fd
Gaurav Rawal <gaurav.rawal@unilodgers.com>
2019-10-02 19:29:57 +0530

    redirect component changes

--- src/containers/Redirect/index.js
+++ src/containers/Redirect/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 function Redirect(props) {
-  return (
-    <div>
-      Redirect
-    </div>
-  )
+	const {
+		match: {
+			params: { id }
+		}
+	} = props;
+
+	useEffect(() => {
+		async function getCorrespondingUrl(id) {
+			// retrieve data from firebase and redirect on the url stored in the db
+		}
+		getCorrespondingUrl(id);
+	}, [id]);
+	return <div>Redirect Component</div>;
 }
 
 Redirect.propTypes = {
-
+	match: PropTypes.shape({
+		params: PropTypes.shape({
+			id: PropTypes.string.isRequired
+		}).isRequired
+	}).isRequired
 };
 
 export default Redirect;
-
